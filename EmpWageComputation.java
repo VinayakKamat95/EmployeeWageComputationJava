44b2409c84f66d1e6a57bbc9d7928ecae6961181
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-05 08:16:29 +0530

    Added : Calculation Wages till a condition of totalworking hours or days is reached for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,34 +5,37 @@ public class EmpWageComputation {
            //CONSTANTS
            // isPartTime = 1
            // isFullTime = 2
-	   int EMP_RATE_PER_HR = 20;
-	   int NUM_WORKING_DAYS = 20;
+      int EMP_RATE_PER_HR = 20;
+      int MAX_HRS_IN_MONTH = 50;
+      int NUM_WORKING_DAYS = 20;
 
-            //Variables
-            int empHrs = 0;
-            int salary = 0;
+       //Variables
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             int totalSalary = 0;
+            int workHrs = 0;
 
-
-       for (int day=1; day<=NUM_WORKING_DAYS; day++){
-      double randomCheck = Math.floor(Math.random() * 10) % 3;
+       while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS){
+        totalWorkingDays+=1;
+   double randomCheck = Math.floor(Math.random() * 10) % 3;
 
          switch ( (int)randomCheck ) {
 
                case 2 :
-                empHrs = 8;
+                workHrs+=8;
             break;
 
                case 1 :
-                empHrs = 4;
+                workHrs+=4;
             break;
 
                default :
-                empHrs = 0;
+                workHrs+=0;
          }
-         salary = empHrs*EMP_RATE_PER_HR;
-         totalSalary=totalSalary+salary;
+         totalEmpHrs=totalEmpHrs+workHrs;
+
       }
+      totalSalary=EMP_RATE_PER_HR*totalEmpHrs;
       System.out.println(totalSalary);
    }
 }
