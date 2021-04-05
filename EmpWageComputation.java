91a66e7e7d2e51ef35418a8ed3b33ef9a02a5c6e
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-05 08:01:10 +0530

    Added : Part Time Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,21 +1,27 @@
 public class EmpWageComputation {
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 
-		//CONSTANTS
-		int IS_PRESENT = 1;
-		//Variables
-		int empRatePerHr = 0;
-		int empHrs = 0;
-		int salary = 0;
-		double randomCheck = Math.floor(Math.random() * 10) % 2;
+           //CONSTANTS
+            int isPartTime = 1;
+            int isFullTime = 2;
 
-		if (randomCheck == IS_PRESENT){
-			empRatePerHr = 20;
-			empHrs = 8;
-			salary = empHrs*empRatePerHr;}
-		else
-			salary=0;
-		System.out.println(salary);
-		}
+            //Variables
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int salary = 0;
+            double randomCheck = Math.floor(Math.random() * 10) % 3;
+
+            if (randomCheck == isFullTime)
+               empHrs = 8;
+
+            else if (randomCheck == isPartTime)
+               empHrs = 4;
+
+            else
+               empHrs = 0;
+
+            salary = empHrs*empRatePerHr;
+            System.out.println(salary);
+   }
 }
