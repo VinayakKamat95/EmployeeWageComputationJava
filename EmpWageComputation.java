89317dfaee6256bba2f16f69e3a906223bab2510
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-05 07:56:55 +0530

    Added : Daily Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,11 +4,18 @@ public class EmpWageComputation {
 
 		//CONSTANTS
 		int IS_PRESENT = 1;
+		//Variables
+		int empRatePerHr = 0;
+		int empHrs = 0;
+		int salary = 0;
 		double randomCheck = Math.floor(Math.random() * 10) % 2;
 
-		if (randomCheck == IS_PRESENT)
-			System.out.println("Employee is Present");
+		if (randomCheck == IS_PRESENT){
+			empRatePerHr = 20;
+			empHrs = 8;
+			salary = empHrs*empRatePerHr;}
 		else
-			System.out.println("Employee is Absent");
-	}
+			salary=0;
+		System.out.println(salary);
+		}
 }
