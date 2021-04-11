b18029fe3c67834a238376b02af5fec5c72eb4bb
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-11 16:41:44 +0530

    Refactor: Compute Employee Wage For Multiple Companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,9 +3,6 @@ public class EmpWageComputation{
    //Constants
    public static final int IS_PART_TIME = 1;
    public static final int IS_FULL_TIME = 2;
-   //public static final int EMP_RATE_PER_HOUR = 20;
-   //public static final int NUM_OF_WORKING_DAYS = 20;
-   //public static final int MAX_HRS_IN_MONTH = 50;
 
    public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
        //Variables
